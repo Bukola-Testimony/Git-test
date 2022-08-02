d47b8a4f7526117bb69ea96069ec78f99f42df93
testimony01 <bukolatestimony2@gmail.com>
2022-08-02 23:21:53 +0100

    changed readme

---
+++ tim.py
@@ -0,0 +1,24 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+
+print("Hello world")
+
+
+operations = {
+   "+": add,
+   "-" :"substract",
+   "*" :"multiply",
+   "/" :"divide",
+   "%" :"modulo"
+ }
+
+n1 = int(input("what is the first number?: ")) 
+  
+for symbols in operations:
+    print(symbols)
+    
+n2 = int(input("what is the first number?: "))
+print(f"The first number is {n1}, second number is {n2}. Answer = {add(n1,n2)}")
+print(add(n1,n2))
