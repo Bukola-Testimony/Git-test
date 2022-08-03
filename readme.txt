38227ba09ef7bd012915890f080765f0760a2ea6
testimony01 <bukolatestimony2@gmail.com>
2022-08-03 13:32:45 +0100

    new changes made

--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
+This is the (master branch) of Repository Git-test.
+
 This is my first readme file for my git.
+Learming Git is fun.
+I am pumped
--- tim.py
+++ tim.py
@@ -1,3 +1,5 @@
+# This is the master branch of repository Git-test on my local machine.
+# Learning git is fun!
 def add(n1, n2):
     return n1 + n2
 
@@ -20,7 +22,7 @@ for symbols in operations:
     print(symbols)
     
 n2 = int(input("what is the first number?: "))
-print(f"The first number is {n1}, second number is {n2}. Answer = {add(n1,n2)}")
+print(f"The first number is {n1}, second number is {n2}. Addition = {add(n1,n2)}")
 print(add(n1,n2))
 
 print("Great experiment")
